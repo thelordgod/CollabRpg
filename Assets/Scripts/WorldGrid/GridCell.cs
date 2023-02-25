0702b4e9f1908260fc95f767bf6364c04bdc34ba
armandsgarancs <Armands.Garancs@eschergroup.com>
2023-02-25 22:07:48 +0200

    start of new adventure

---
+++ Assets/Scripts/WorldGrid/GridCell.cs
@@ -0,0 +1,18 @@
+using System;
+using UnityEngine;
+
+namespace WorldGrid
+{
+    public class GridCell : MonoBehaviour
+    {
+        private void Start()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void Update()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- Assets/Scripts/WorldGrid/WorldGridMap.cs
+++ Assets/Scripts/WorldGrid/WorldGridMap.cs
@@ -1,9 +1,35 @@
+using System;
 using UnityEngine;
 
 namespace WorldGrid
 {
     public class WorldGridMap : MonoBehaviour
     {
+
+        [SerializeField] private int width;
+        [SerializeField] private int height;
+        private GridCell[] allCells;
         
+        private void Start()
+        {
+            allCells = new GridCell[width * height];
+            for (int i = 0; i < height; i++)
+            {
+
+
+            }
+
+            print("Sveiki");
+        }
+
+        public void GetCellAt(int x, int y)
+        {
+            
+        }
+
+        private void Update()
+        {
+
+        }
     }
 }
